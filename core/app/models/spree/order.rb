5d4cbee8e3f24874ebbd8412020e619b30e64b66
David Ang <davidang09@gmail.com>
2015-12-16 23:30:23 +0800

    do not remove the transition from delivery to confirm - we need users to still confirm for free orders

--- core/app/models/spree/order.rb
+++ core/app/models/spree/order.rb
@@ -18,7 +18,6 @@ module Spree
       go_to_state :payment, if: ->(order) { order.payment_required? }
       go_to_state :confirm, if: ->(order) { order.confirmation_required? }
       go_to_state :complete
-      remove_transition from: :delivery, to: :confirm
     end
 
     attr_reader :coupon_code
